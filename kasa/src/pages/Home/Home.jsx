35fc889f2a8370e2af297cafd5f409c7a0e56626
dague <daniel.guedj.pro@gmail.com>
2024-08-12 12:59:20 +0200

    ADD: banner home

--- kasa/src/pages/Home/Home.jsx
+++ kasa/src/pages/Home/Home.jsx
@@ -4,6 +4,12 @@ import './Home.css'
 
 function Home() {
   return (
+    <div className="home-container">
+      <div className="banner-container">
+        <div className="banner-text-container">
+          <div className="banner-text">Chez vous, partout et ailleurs</div>
+        </div>
+      </div>
       <div className="cards-container">
         {logements.map((accommodation, index) => (
           <Card
@@ -14,6 +20,7 @@ function Home() {
           />
         ))}
       </div> 
+    </div>
     )
 }
 
